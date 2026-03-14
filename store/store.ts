5acb0bb9f07db312e3918f9497674e4e307f86f7
isaac-kg <isaackagiso7@gmail.com>
2026-03-14 11:27:06 +0200

    redux

--- store/store.ts
+++ store/store.ts
@@ -1,15 +1,44 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
 import { authApi } from '@/features/auth/auth-api';
 import { authSlice } from '@/features/auth/auth-slice';
 
-export const store = configureStore({
-  reducer: {
-    [authApi.reducerPath]: authApi.reducer,
-    auth: authSlice.reducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+function createNoopStorage() {
+  return {
+    getItem: () => Promise.resolve(null as string | null),
+    setItem: (_key: string, value: unknown) => Promise.resolve(value),
+    removeItem: () => Promise.resolve(),
+  };
+}
+
+const storage =
+  typeof window !== 'undefined'
+    ? require('redux-persist/lib/storage').default
+    : createNoopStorage();
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(authPersistConfig, authSlice.reducer);
+
+const rootReducer = combineReducers({
+  [authApi.reducerPath]: authApi.reducer,
+  auth: persistedAuthReducer,
 });
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }).concat(authApi.middleware),
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
